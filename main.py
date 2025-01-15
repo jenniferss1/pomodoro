d0edddcc3fe77efcf473b5590acddfabdc05db03
jennifer <jennifersantossouza@hotmail.com>
2025-01-15 17:40:27 -0300

    feat: implementing the reset button and adjusting the layout

--- main.py
+++ main.py
@@ -1,20 +1,25 @@
 from tkinter import *
 import math
 
-# ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = NONE
 
-#TIMER RESET
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title.config(text="Timer")
+    checks.config(text="")
+    global reps
+    reps = 0
 
-#TIMER MECHANISM 
 def start_timer():
     global reps
     reps +=1
@@ -34,7 +39,6 @@ def start_timer():
         title.config(text="Work", fg=GREEN)
     
 
-#COUNTDOWN MECHANISM 
 def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
@@ -43,14 +47,19 @@ def count_down(count):
     
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
     else:
         start_timer()
+        mark = ""
+        work_session = math.floor(reps/2)
+        for _ in range(work_session):
+            mark += "✅"
+        checks.config(text=mark)
 
-#UI SETUP
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=100, pady=50, bg=YELLOW)
+window.config(padx=60, pady=10, bg=YELLOW)
 
 title = Label(text="Timer", font=(FONT_NAME, 50, "bold"), bg=YELLOW, fg=GREEN)
 title.grid(column=1, row=0)
@@ -65,10 +74,10 @@ canvas.grid(column=1, row=1)
 start_button = Button(text="Start", font=(FONT_NAME, 10, "bold"), command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", font=(FONT_NAME, 10, "bold"))
+reset_button = Button(text="Reset", font=(FONT_NAME, 10, "bold"), command=reset_timer)
 reset_button.grid(column=2, row=2)
 
-checks = Label(text="✅", bg=YELLOW, fg=GREEN)
+checks = Label(bg=YELLOW, fg=GREEN)
 checks.grid(column=1, row=3)
 
 window.mainloop()
